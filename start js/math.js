926204599fb2cb38d25fa51697bdf05c01470255
ydgin <119066979+ydgin@users.noreply.github.com>
2024-01-20 19:07:03 +0300

    new commit

---
+++ start js/math.js
@@ -0,0 +1,16 @@
+// функция для сложения;
+function sum(a,b) {
+    return a + b;
+}
+// функция для вычитания;
+function substract(a,b) {
+    return a - b;
+}
+// функция для умножения;
+function multiply(a,b) {
+    return a * b;
+}
+// функция деления;
+function divison(a,b) {
+    return a / b;
+}
